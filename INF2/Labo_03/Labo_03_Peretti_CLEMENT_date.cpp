094d02953750efb0acf58fec8f034b4ac7bc8081
galedric <g@ledric.me>
2015-03-16 16:35:13 +0100

    Updated Date interface

--- INF2/Labo_03/Labo_03_Peretti_CLEMENT_date.cpp
+++ INF2/Labo_03/Labo_03_Peretti_CLEMENT_date.cpp
@@ -14,6 +14,8 @@
  */
 
 #include "Labo_03_Peretti_CLEMENT_date.h"
+#include <iostream>
+#include <sstream>
 using namespace std;
 
 const std::string TAB_MOIS[] = {
@@ -21,3 +23,58 @@ const std::string TAB_MOIS[] = {
 	"juin", "juillet", "aout", "septembre",
 	"octobre", "novembre", "decembre"};
 
+// Constructeurs
+// Equivalents à l'appel de la méthode setDate correspondante
+Date::Date(int jour, Mois mois, int annee) { setDate(jour, mois, annee); }
+Date::Date(const string& str) { setDate(str); }
+
+// Getters
+int Date::getJour() const { return jour; }
+Mois Date::getMois() const { return mois; }
+int Date::getAnnee() const { return annee; }
+
+// Setter pour une date complète (format jour / mois / annee)
+// Appel les méthodes setJour, setMois et setAnnee
+Date& Date::setDate(int jour, Mois mois, int annee) {
+	return setJour(jour), setMois(mois), setAnnee(annee);
+}
+
+// Setter pour une date complète (format chaine de caractères)
+Date& Date::setDate(const string& str) {
+	return *this;
+}
+
+Date& Date::setJour(int j) {
+	jour = j;
+	return *this;
+}
+
+Date& Date::setMois(Mois m) {
+	mois = m;
+	return *this;
+}
+
+Date& Date::setAnnee(int a) {
+	annee = a;
+	return *this;
+}
+
+// Véréfie si la date est valide
+bool Date::isValid() const {
+	return false;
+}
+
+// Retourne une représentation textuelle de la date
+string Date::toString() const {
+	ostringstream oss;
+	oss << jour << " " << mois << " " << annee;
+	return oss.str();
+}
+
+// Conversion en d'autres types
+Date::operator bool() const { return isValid(); }
+Date::operator string() const { return toString(); }
+
+// Surcharge des opértaeurs de flux
+ostream& operator << (ostream& os, Mois m) { return os << TAB_MOIS[(int) m]; }
+ostream& operator << (ostream& os, const Date& date) { return os << date; }
--- INF2/Labo_03/Labo_03_Peretti_CLEMENT_date.h
+++ INF2/Labo_03/Labo_03_Peretti_CLEMENT_date.h
@@ -8,6 +8,7 @@
 #ifndef LABO_03_PERETTI_CLEMENT_DATE_H
 #define	LABO_03_PERETTI_CLEMENT_DATE_H
 #include <string>
+#include <iostream>
 
 enum class Mois {
 	janvier, fevrier, mars, avril, mai, juin, juillet,
@@ -21,23 +22,35 @@ private:
 	int annee;
 	
 public:
-	Date(int jour, Mois mois, int annee);
-	Date(std::string str);
-	
-	int getJour();
-	Mois getMois();
-	std::string getMoisStr();
-	int getAnnee();
-	
-	void setJour(int jour);
-	void setMois(Mois mois);
-	void setMoisStr(std::string str);
-	void setAnnee(int annee);
-	
-	std::string toString();
-	void fromString(std::string str);
+	// Constructeurs
+	Date(int jour = 1, Mois mois = Mois::janvier, int annee = 2000);
+	Date(const std::string& str);
+
+	// Getters
+	int getJour() const;
+	Mois getMois() const;
+	int getAnnee() const;
+
+	// Setters pour la date complète
+	Date& setDate(int jour, Mois mois, int annee);
+	Date& setDate(const std::string& str);
+
+	// Setters pour un membre particulier
+	Date& setJour(int jour);
+	Date& setMois(Mois mois);
+	Date& setAnnee(int annee);
+
+	// Vérifie la validité d'une date
+	bool isValid() const;
+	operator bool() const;
+
+	// Retourne la représentation textuelle d'une date
+	std::string toString() const;
+	operator std::string() const;
 };
 
+std::ostream& operator << (std::ostream& os, Mois mois);
+std::ostream& operator << (std::ostream& os, const Date& date);
 
 #endif	/* LABO_03_PERETTI_CLEMENT_DATE_H */
 
