6242bbe518e4adeb3a12bcd29c1ad581ab0baf6e
galedric <g@ledric.me>
2015-03-23 17:17:16 +0100

    Implement DequeStatique (not tested)

--- INF2/Labo_04/main.cpp
+++ INF2/Labo_04/main.cpp
@@ -10,9 +10,13 @@ private:
 	size_t debut;
 	size_t elements = 0;
 
+	Element& get(size_t index);
+
 public:
 	DequeStatique(size_t taille = 0);
 	~DequeStatique();
+
+	void resize(size_t taille);
 
 	bool push_back(Element& e);
 	bool pop_back(Element& e);
@@ -22,8 +26,6 @@ public:
 	bool estPleine() const;
 	bool estVide() const;
 	bool estPresent(const Element& e) const;
-	
-	void resize(size_t taille);
 };
 
 DequeStatique::DequeStatique(size_t t) {
@@ -37,7 +39,11 @@ DequeStatique::~DequeStatique() {
 	delete[] data;
 }
 
-// Redimensionnement de la Deque statique
+Element& DequeStatique::get(size_t index) {
+	return data[(debut + index) % taille];
+}
+
+// Redimensionnement du deque statique
 void DequeStatique::resize(size_t t) {
 	// Taille identique
 	if (t == taille) return;
@@ -52,8 +58,8 @@ void DequeStatique::resize(size_t t) {
 
 	// Il y a des éléments à copier
 	if (data) {
-		for (size_t i = 0, j = debut; i < elements; i++, j++) {
-			new_data[i] = data[j % taille];
+		for (size_t i = 0; i < elements; i++) {
+			new_data[i] = get(i);
 		}
 
 		// Supression de l'ancien buffer
@@ -63,6 +69,47 @@ void DequeStatique::resize(size_t t) {
 	data = new_data;
 	debut = 0;
 	taille = t;
+}
+
+bool DequeStatique::push_back(Element& e) {
+	if (estPleine()) return false;
+	get(elements++) = e;
+	return true;
+}
+
+bool DequeStatique::pop_back(Element& e) {
+	if (estVide()) return false;
+	e = get(--elements);
+	return true;
+}
+
+bool DequeStatique::push_front(Element& e) {
+	if (estPleine()) return false;
+	debut = (debut + taille - 1) % taille;
+	get(0) = e;
+	return true;
+}
+
+bool DequeStatique::pop_front(Element& e) {
+	if (estVide()) return false;
+	e = get(0);
+	debut = (debut + 1) % taille;
+	return true;
+}
+
+bool DequeStatique::estPleine() const {
+	return elements == taille;
+}
+
+bool DequeStatique::estVide() const {
+	return elements == 0;
+}
+
+bool DequeStatique::estPresent(const Element &e) const {
+	for (size_t i = 0; i < elements; i++) {
+		if (data[(debut + i) % taille] == e) return true;
+	}
+	return false;
 }
 
 // =================================================================================================
