a92fb2a73cd5d9f7422b0ac4d76d279f001ff5cc
Christophe Peretti <chris.peretti@gmail.com>
2015-04-17 13:41:41 +0200

    base pour tris (labo3)

---
+++ ASD1/Labo_03/labo3.cpp
@@ -0,0 +1,26 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire    : ASD_Labo_3
+ Fichier        : labo3.cpp
+ Auteur(s)      : Christophe Peretti et Bastien Clément
+ -----------------------------------------------------------------------------------
+ */
+
+#include <vector>
+
+using namespace std;
+
+typedef unsigned char byte;
+
+// Nous faisons des passages par copie et non par référence afin de pouvoir
+// passer le même tableau à chaque fonction de tri
+void selectionSort(vector<byte> T);
+void countingSort(vector<byte> T);
+void quickSort(vector<byte> T);
+
+
+int main(int argc, const char * argv[]) {
+
+
+    return 0;
+}
