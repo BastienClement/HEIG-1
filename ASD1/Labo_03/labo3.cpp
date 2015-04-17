c122e2473c68d6d3285e2dda2c93b6897f1bd2d2
galedric <g@ledric.me>
2015-04-17 14:11:35 +0200

    Randomize and countingSort

--- ASD1/Labo_03/labo3.cpp
+++ ASD1/Labo_03/labo3.cpp
@@ -7,19 +7,49 @@
  */
 
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
 
 typedef unsigned char byte;
 
-// Nous faisons des passages par copie et non par référence afin de pouvoir
-// passer le même tableau à chaque fonction de tri
-void selectionSort(vector<byte> T);
-void countingSort(vector<byte> T);
-void quickSort(vector<byte> T);
+// Non-inclus
+const byte VAL_MAX = 100;
 
+void selectionSort(vector<byte>& T);
+void countingSort(vector<byte>& T);
+void quickSort(vector<byte>& T);
 
-int main(int argc, const char *argv[]) {
+void randomize(vector<byte>& vec, size_t items);
+
+int main(int argc, const char* argv[]) {
+	// Initialisation de l'aléatoire
+	srand(time(NULL));
 
 	return 0;
 }
+
+void randomize(vector<byte>& vec, size_t items) {
+	vec.clear();
+	for (size_t i = 0; i < items; i++) {
+		vec.push_back(rand() % VAL_MAX);
+	}
+}
+
+void countingSort(vector<byte>& T) {
+	// Tableau de comptage
+	unsigned int count[VAL_MAX] = {};
+
+	// Comptage des occurences
+	for_each(T.begin(), T.end(), [&](byte v) { count[v]++; });
+
+	// On vide le tableau original
+	T.clear();
+
+	// On le reconstruit dans l'ordre
+	for (size_t i = 0; i < 100; i++) {
+		for (size_t n = count[i]; n > 0; n--) {
+			res.push_back(i);
+		}
+	}
+};
