17205d830003b9ac7c64bd260cc9f7db9bd4cbc4
Christophe Peretti <chris.peretti@gmail.com>
2015-04-22 13:50:40 +0200

    matrice

---
+++ INF2/Labo_06-chris/Labo_06_CotzaPeretti_aleat.cpp
@@ -0,0 +1,19 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire    : Labo_06
+ Fichier        : Labo_06_CotzaPeretti_aleat.cpp
+ Auteur(s)      : Christophe Peretti et Bastien Clément
+ Date           : 02 mars 2015
+ 
+ Remarques(s)   :
+ 
+ Compilateur    : MinGW-g++ 4.8.1
+ -----------------------------------------------------------------------------------
+ */
+#include <iostream>
+#include "Labo_06_CotzaPeretti_aleat.h"
+using namespace std;
+
+Matrice genererMatrice(size_t taille){
+	
+}
