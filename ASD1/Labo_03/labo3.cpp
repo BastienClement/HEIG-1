b7b7caf24274f30609ca8a062740ef50668dbaf4
Christophe Peretti <chris.peretti@gmail.com>
2015-04-24 14:38:01 +0200

    iterator_traits

--- ASD1/Labo_03/labo3.cpp
+++ ASD1/Labo_03/labo3.cpp
@@ -43,19 +43,18 @@ void selectionSort(RandomAccessIterator begin, RandomAccessIterator end) {
 template<typename RandomAccessIterator>
 void countingSort(RandomAccessIterator begin, RandomAccessIterator end) {
 	// Type de la valeur pointée par l'itérateur
-	typedef typename RandomAccessIterator::value_type val;
+	typedef typename iterator_traits<RandomAccessIterator>::value_type val;
 
 	// Recherche de l'élément le plus petit et le plus grand
 	val min = numeric_limits<val>::max(), max = numeric_limits<val>::min();
-
-	for (RandomAccessIterator i = begin; i < end; i++) {
-		if (*i < min) min = *i;
-		if (*i > max) max = *i;
-	}
+	for_each(begin, end, [&](val v) {
+		if (v < min) min = v;
+		if (v > max) max = v;
+	});
 
 	// Tableau de comptage
 	const size_t range = max - min + 1;
-	vector<val> count(range, 0);
+	vector<unsigned> count(range, 0);
 
 	// Comptage des occurences
 	for_each(begin, end, [&](val v) { count[v - min]++; });
@@ -102,7 +101,7 @@ void quickSort(RandomAccessIterator begin, RandomAccessIterator end) {
 		while (true) {
 			do {
 				i++;
-			} while (*i < *hi);
+			} while (i < end && *i < *hi);
 
 			do {
 				j--;
@@ -136,31 +135,34 @@ int main(int argc, const char* argv[]) {
 
 	for (int i = 2; i < 6; i++) {
 		const int iter = 5;
-		double select_time = 0, counting_time = 0, quick_time = 0;
+		double counting_time = 0, quick_time = 0, select_time = 0;
 
 		for (int j = 0; j < iter; j++) {
 			// Tableau de référence
 			vector<byte> R;
 			randomize(R, pow(10, i));
 
-			// Copie de travail pour les 3 algorithm
-			vector<byte> S = R;
 			vector<byte> C = R;
-			vector<byte> Q = R;
-
-			select_time += benchmark([&]() { selectionSort(S.begin(), S.end()); });
 			counting_time += benchmark([&]() { countingSort(C.begin(), C.end()); });
-			quick_time += benchmark([&]() { quickSort(Q.begin(), Q.end()); });
 
-			// On vérifie que les trois tris ont produit le même résultat
-			assert(S == C && C == Q);
+			if (i < 9) {
+				vector<byte> Q = R;
+				quick_time += benchmark([&]() { quickSort(Q.begin(), Q.end()); });
+				cout << (Q == C);
+			}
+
+			if (i < 5) {
+				vector<byte> S = R;
+				select_time += benchmark([&]() { selectionSort(S.begin(), S.end()); });
+				assert(S == C);
+			}
 		}
 
 		select_time /= iter;
 		counting_time /= iter;
 		quick_time /= iter;
 
-		printf("%d\t%lf\t%lf\t%lf\n", i, select_time, counting_time, quick_time);
+		printf("%d\t%f\t%f\t%f\n", i, counting_time, quick_time, select_time);
 		fflush(stdout);
 	}
 
