16741bf03d457e8f9116c9de6e2ff60c6a132b3e
Christophe Peretti <chris.peretti@gmail.com>
2015-04-27 13:14:36 +0200

    change main

--- INF2/Labo_06-chris/Labo_06_CotzaPeretti.cpp
+++ INF2/Labo_06-chris/Labo_06_CotzaPeretti.cpp
@@ -5,19 +5,33 @@ using namespace std;
 
 
 int main() {
-	char fileName[20];
+	string fileName;
 	size_t x,y;
 	unsigned int nbreCellules;
-	cout << "Entrez le nom du fichier a generer : ";
-	cin >> fileName;
-   cout << "Entrez la largeur du tableau :";
-	cin >> x;
-	cout << "Entrez la hauteur du tableau :";
-	cin >> y;
-	cout << "Entrez le nombre de case a modifier (" << x*y << " max):";
-	cin >> nbreCellules;
-	Matrice m = genererMatrice(x,y);
-	remplirMatrice(m, nbreCellules);
-    arrayToHtml(fileName, m);
-    return 0;
+	char onRecommence;
+	
+	do{
+		do{
+			cout << "Entrez le nom du fichier a generer (20 car max): ";
+			cin >> fileName;
+		} while (fileName.size() > 20);
+	
+		cout << "Entrez la largeur du tableau :";
+		cin >> x;
+		cout << "Entrez la hauteur du tableau :";
+		cin >> y;
+		
+		do{
+		cout << "Entrez le nombre de case a modifier (" << x*y << " max):";
+		cin >> nbreCellules;
+		} while (nbreCellules > x*y);
+		
+		Matrice m = genererMatrice(x,y);
+		remplirMatrice(m, nbreCellules);
+		arrayToHtml(fileName, m);
+	
+		cout << "Voulez-vous recommencer? [o/n]";
+		cin >> onRecommence;
+	} while(onRecommence == 'o' || onRecommence == 'O');
+   return 0;
 }
