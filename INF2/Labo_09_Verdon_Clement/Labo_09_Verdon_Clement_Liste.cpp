9ec662cd2e7a728ef8c29f523027412cb5926142
galedric <g@ledric.me>
2015-05-19 17:18:52 +0200

    Implémentation Liste<T>

--- INF2/Labo_09_Verdon_Clement/Labo_09_Verdon_Clement_Liste.cpp
+++ INF2/Labo_09_Verdon_Clement/Labo_09_Verdon_Clement_Liste.cpp
@@ -3,3 +3,129 @@
 //
 
 #include "Labo_09_Verdon_Clement_Liste.h"
+
+template <typename T>
+Liste<T>::Liste() {
+	debut = new Element<T>;
+	fin = new Element<T>;
+
+	*debut = { nullptr, fin };
+	*fin = { debut, nullptr };
+}
+
+template <typename T>
+Liste<T>::~Liste() {
+	delete debut;
+	delete fin;
+}
+
+template <typename T>
+Liste<T>& Liste<T>::operator=(const Liste<T>& liste) {
+	if (&liste != this) {
+		vider();
+		liste.parcourir([this](T el) {
+			inserer(el);
+		});
+	}
+}
+
+template <typename T>
+bool Liste<T>::existe(T element) const {
+	for (auto cur = debut->suivant; cur != fin; cur = cur->suivant) {
+		if (cur->donnee == element) {
+			return true;
+		}
+	}
+	return false;
+}
+
+template <typename T>
+void Liste<T>::inserer(T valeur) {
+	inserer(valeur, fin->precedent);
+}
+
+template <typename T>
+bool Liste<T>::inserer(T valeur, Element<T>* apres) {
+	Element<T>* element = new Element<T>;
+	*element = { apres, apres->suivant, valeur };
+	apres->suivant->precedent = element;
+	apres->suivant = element;
+}
+
+template <typename T>
+void Liste<T>::inserer(T valeur, size_t pos) {
+	auto element = debut;
+	for (size_t i = pos; i > 0 && element->suivant != fin; i--) {
+		element = element->suivant;
+	}
+	inserer(valeur, element);
+}
+
+template <typename T>
+void Liste<T>::inserer(T valeur, Identifieur<T> iden) {
+	auto element = debut;
+	while (element->suivant != fin && iden(element->suivant->donnee)) {
+		element = element->suivant;
+	}
+	inserer(valeur, element);
+}
+
+template <typename T>
+void Liste<T>::supprimer(Element<T>* element) {
+	if (element == debut || element == fin)
+		throw ErreurListe { "Impossible de supprimer l'élément de début ou de fin" };
+
+	element->precedent->suivant = element->suivant;
+	element->suivant->precedent = element->precedent;
+
+	delete element;
+}
+
+template <typename T>
+void Liste<T>::supprimer(Identifieur<T> iden) {
+	for (auto cur = debut->suivant; cur != fin; cur = cur->suivant) {
+		if (iden(cur->donnee)) {
+			cur = cur->precedent;
+			supprimer(cur->suivant);
+		}
+	}
+}
+
+template <typename T>
+void Liste<T>::supprimer(size_t pos) {
+	auto element = debut->suivant;
+	for (size_t i = pos; i > 0 && element != fin; i--) {
+		element = element->suivant;
+	}
+	supprimer(element);
+}
+
+template <typename T>
+void Liste<T>::vider() {
+	while (debut->suivant != fin) {
+		supprimer(debut->suivant);
+	}
+}
+
+template <typename T>
+void Liste<T>::parcourir(Parcoureur<T> par) const {
+	for (auto cur = debut->suivant; cur != fin; cur = cur->suivant) {
+		par(cur->donnee);
+	}
+}
+
+template <typename T>
+void Liste<T>::trier(Comparateur<T> comp) {
+	// TODO
+}
+
+template <typename T>
+void Liste<T>::swap(Element<T>* a, Element<T>* b) {
+	if (a == debut || a == fin || b == debut || b == fin)
+		throw ErreurListe { "Impossible d'inverser l'élément de début ou de fin" };
+
+	std::swap(a->precedent, b->precedent);
+	std::swap(a->suivant, b->suivant);
+	std::swap(a->precedent->suivant, b->precedent->suivant);
+	std::swap(a->suivant->precedent, b->suivant->precedent);
+}
--- INF2/Labo_09_Verdon_Clement/Labo_09_Verdon_Clement_Liste.h
+++ INF2/Labo_09_Verdon_Clement/Labo_09_Verdon_Clement_Liste.h
@@ -6,30 +6,51 @@
 #define LABO_09_VERDON_CLEMENT_LISTE_H
 
 #include <cstdlib>
+#include <functional>
+#include <string>
 #include "Labo_09_Verdon_Clement_Element.h"
 
-template <typename T>
-using Comparateur = bool (*)(const T& a, const T& b);
+struct ErreurListe {
+	std::string message;
+};
 
 template <typename T>
-using Parcoureur = void (*)(const T& a);
+using Comparateur = bool (*)(const T&, const T&);
 
+template <typename T>
+using Identifieur = bool (*)(const T&);
+
+template <typename T>
+using Parcoureur = std::function<void(T)>;
 
 template <typename T>
 class Liste {
 private:
-	Element<T>* tete;
+	Element<T>* debut;
+	Element<T>* fin;
+
+	bool inserer(T valeur, Element<T>* apres = nullptr);
+	void supprimer(Element<T>* element);
+	void swap(Element<T>* a, Element<T>* b);
 
 public:
-	Liste& operator=(const Liste& liste);
+	Liste();
+	~Liste();
+
+	Liste<T>& operator=(const Liste<T>& liste);
+
 	bool existe(T element) const;
-	bool inserer(T element, size_t pos);
-	bool inserer(T element, Comparateur<T> comp);
-	bool supprimer(Comparateur<T> comp);
-	bool supprimer(size_t pos);
-	bool vider();
-	void parcourir(Parcoureur<T> comp) const;
-	bool trier(Comparateur<T> comp);
+
+	void inserer(T valeur);
+	void inserer(T valeur, size_t pos);
+	void inserer(T valeur, Identifieur<T> iden);
+
+	void supprimer(Identifieur<T> iden);
+	void supprimer(size_t pos);
+
+	void vider();
+	void parcourir(Parcoureur<T> par) const;
+	void trier(Comparateur<T> comp);
 };
 
 #endif
