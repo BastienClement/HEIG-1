e8356db33c98bb8070aae4f9ba3e2ff166242140
verdonarthur <verdon.arthur@gmail.com>
2015-05-20 10:36:37 +0200

    Load and save file dictionnary

--- INF2/Labo_09_Verdon_Clement/Labo_09_Verdon_Clement_Dictionnaire.cpp
+++ INF2/Labo_09_Verdon_Clement/Labo_09_Verdon_Clement_Dictionnaire.cpp
@@ -3,4 +3,41 @@
 //
 
 #include "Labo_09_Verdon_Clement_Dictionnaire.h"
+#include <fstream>
 
+void Dictionnaire::sauver(const string& nom) const {
+
+    ofstream fichier(nom, ios::out | ios::trunc);
+
+    if (fichier.is_open()) {
+        for(Element i = mots.tete; i.suivant != nullptr; i++) {
+            fichier << i.donnee;
+        }
+    }
+}
+
+void Dictionnaire::charger(const string& nom, bool trier = false) {
+
+    string mot;
+
+	ifstream fichier(nomFichier, ios::in);
+
+	if (fichier.is_open()) {
+		while (fichier >> mot) {
+			this->mots->inserer(mot,0);
+		}
+
+		if(trier){
+            this->mots->trier();
+		}
+	}
+
+}
+
+void Dictionnaire::afficher() const {
+
+}
+
+void Dictionnaire::corriger(const string& nom) const {
+
+}
